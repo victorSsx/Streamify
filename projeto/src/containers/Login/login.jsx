bbe1694a38c8683f4ccc11d3f9912709948a5107
DEVictor <126361107+victorSsx@users.noreply.github.com>
2024-11-16 22:23:51 +0000

    retoque no css

--- projeto/src/containers/Login/login.jsx
+++ projeto/src/containers/Login/login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import './login.css';
+
 const Login = () => {
   const [username, setUsername] = useState(''); // Correção: usar useState corretamente
   const [password, setPassword] = useState(''); // Correção: usar useState corretamente
@@ -22,25 +24,26 @@ const Login = () => {
     <div>
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
-        <label>
-          Username
+        
           <input
+            placeholder='Nome do Usuário'
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
           />
-        </label>
+        
         <br />
-        <label>
-          Password
+      
+        
           <input
+            placeholder='Senha'
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </label>
+
         <br />
         <button type="submit">Entrar</button>
       </form>
