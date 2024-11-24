acf6257afa8ddc556e1bdaf22d1340a2c1b96c28
DEVictor <126361107+victorSsx@users.noreply.github.com>
2024-11-24 18:25:32 +0000

    ave maria cheia de graça

--- projeto/src/containers/Home/Banner/banner.jsx
+++ projeto/src/containers/Home/Banner/banner.jsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react'
+const vite_img = import.meta.env.VITE_API_IMG
+const apiKey = import.meta.env.VITE_API_KEY
+
 import './banner.css'
 
 const Banner = () => {
 
+    const [banner, setBanner] = ([])
+
+    const getImgBanner = async (url) => {
+        const res = await feach (url)
+        const data = await res.json()
+
+        setBanner(data.results)
+    }
+    useEffect(() => {
+        const imgBanner = `${vite_img}img_banner?${apiKey}`
+
+        getImgBanner(imgBanner)
+    }, [])
+
     return(
         <>
         <div className='banner_p'>
-            <li></li>
         </div>
         </>
     )
